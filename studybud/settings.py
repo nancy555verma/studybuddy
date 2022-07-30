7749cfcf7759d34bc4ff5da9579214a53ddb960c
Nancy Verma <nancy555verma@gmail.com>
2022-07-31 03:04:56 +0530

    heroku static

--- studybud/settings.py
+++ studybud/settings.py
@@ -132,17 +132,16 @@ USE_TZ = True
 #     BASE_DIR / 'static'
 # ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print(STATIC_ROOT)
 # STATIC_URL = 'static/'
 
 # STATIC_ROOT = '/'
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-    ]
+STATICFILES_DIRS = []
 
-django_heroku.settings(locals())
+django_heroku.settings(locals(), staticfiles=False)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
